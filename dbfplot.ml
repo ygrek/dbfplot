93abbc1725f5e3ead2d8994765e791b2adafc92c
ygrek <ygrek@autistici.org>
2009-08-11 21:34:26 +0300

    simple text based select

--- dbfplot.ml
+++ dbfplot.ml
@@ -18,24 +18,67 @@ let gnuplot out name cols =
   IO.printf out "plot %s;\n" (String.concat "," (List.map plot_col cols));
   IO.printf out "pause -1;\n"
 
-let process dbf =
-  with_open_out temp_gnuplot (fun ch ->
-    with_output_ch ch (fun out ->
-      gnuplot out temp_csv [5;6;7;8;9]
-  ));
-  Sys.command (sprintf "dbfxtrct -i%s > %s" dbf temp_csv) >> ignore;
-  Sys.command (sprintf "gnuplot %s" temp_gnuplot) >> ignore
+let split_columns s =
+    String.nsplit (String.replace_chars (function ',' -> " " | x -> String.make 1 x) s) " " >>
+    List.map String.strip >>
+    List.filter (fun s -> String.length s > 0)
 
-let usage_msg = 
+let user_select_file () =
+  let l =
+  Sys.readdir "." >> Array.enum >>
+  Enum.filter (fun s -> String.ends_with (String.lowercase s) ".dbf") >>
+  List.of_enum
+  in
+  List.iteri (fun i s -> printf "%u) %s\n" i s) l;
+  print_string "Select file : "; flush stdout;
+  let input = input_line stdin in
+  let name = try let n = int_of_string input in List.nth l n with _ -> input in
+  if Sys.file_exists name then Some name else
+    begin printf "No such file : %s\n%!" name; None end
+
+let dbf_to_csv dbf =
+  Sys.command (sprintf "dbfxtrct -i%s > %s" dbf temp_csv) >> ignore
+
+let user_select_columns name =
+  dbf_to_csv name;
+  bracket (open_in temp_csv) close_in_noerr (fun ch ->
+    let first = try input_line ch with _ -> "" in
+    let cols = split_columns first in
+    if cols <> [] then
+    begin
+      List.iteri (fun i s -> printf "%u) %s\n" i s) cols;
+      print_string "Select columns : "; flush stdout;
+      let input = input_line stdin in
+      let sel = split_columns input >> List.filter_map (fun s -> catch int_of_string s) in
+      Some sel
+    end
+    else begin print_endline "No columns found"; None end
+  )
+
+let process dbf =
+  match user_select_columns dbf with
+  | None -> ()
+  | Some cols ->
+    with_open_out temp_gnuplot (fun ch ->
+      with_output_ch ch (fun out -> gnuplot out temp_csv cols)
+    );
+    dbf_to_csv dbf; (* not needed really *)
+    Sys.command (sprintf "gnuplot %s" temp_gnuplot) >> ignore
+
+let usage_msg =
   let s1 = sprintf "dbfplot ver. %s\n" Git.revision in
   let s2 = sprintf "Usage: %s <file.dbf>\n" (Filename.basename Sys.executable_name) in
   let s3 = "" in
   s1 ^ s2 ^ s3
 
 let main () =
-  if 1 = Array.length Sys.argv then print_string usage_msg;
-  let args = [] in
-  Arg.parse (Arg.align args) process usage_msg
+  if 1 = Array.length Sys.argv then
+    while true do
+      match user_select_file () with Some f -> process f | None -> ()
+    done
+  else
+    let args = [] in
+    Arg.parse (Arg.align args) process usage_msg
 
 let () = Printexc.print main ()
 
--- prelude.ml
+++ prelude.ml
@@ -1,4 +1,5 @@
 
 let bracket res destroy k = Std.finally (fun () -> destroy res) k res
 let (>>) x f = f x
- 
+let catch f x = try Some (f x) with _ -> None
+
